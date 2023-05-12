c7afcc52d10f8c7ce7f3762d8f89a52d6ed961b4
Yeison Hernandez <developer.ss.2@tucash.co>
2023-05-11 21:33:01 -0500

    First commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF49149F);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+];
+
+class AppTheme {
+  ThemeData theme() {
+    return ThemeData(useMaterial3: true, colorSchemeSeed: _colorThemes[5]);
+  }
+}
