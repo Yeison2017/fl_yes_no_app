1d89b9dc694b523dd364902f70ca70b12ff673e1
Yeison <developer.ss.2@tucash.co>
2023-05-20 11:18:43 -0500

    Se maqueta el chat

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+import '../../widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://bodysize.org/wp-content/uploads/2021/01/2edc9d3c-1934-44ba-9304-db6a68f98939-300x300.jpg'),
+          ),
+        ),
+        title: const Text("My love"),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return (index % 2 == 0)
+                          ? const HerMessageBubble()
+                          : const MyMessageBubble();
+                    })),
+
+            /// Caja de texto de mensajes
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
